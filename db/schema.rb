8ce1de72d4fbd7c1354b95a75cddd08f94a0dbc1
kosmo8019 <s.budziosz@hotmail.com>
2018-05-16 10:35:49 +0200

    user działa
    gem bcrypt uninstall
    gem psych ununstall
    gem install bcrypt --platform=ruby

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_14_193835) do
+ActiveRecord::Schema.define(version: 2018_05_16_072314) do
 
   create_table "ksiazkas", force: :cascade do |t|
     t.string "tytul"
@@ -18,6 +18,23 @@ ActiveRecord::Schema.define(version: 2018_05_14_193835) do
     t.string "autor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "uzytkowniks", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_uzytkowniks_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_uzytkowniks_on_reset_password_token", unique: true
   end
 
 end
